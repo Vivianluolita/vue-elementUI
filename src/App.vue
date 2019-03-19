96f27413cd1f6d13f47f4692ba8c85bdf3ba7c37
Vivianluolita <15071362462@163.com>
2019-03-19 11:01:09 +0800

    去除dist

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,8 @@ export default {
     'v-header':header
   },
   created(){
+    var appData = require('../data.json');
+    this.user = appData.user;
     //为了github pages，原接口改为本地数据
     // this.$http.get('/api/user').then((response)=>{
     //   response = response.body;
