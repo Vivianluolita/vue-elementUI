dab7868b0f7cea26f697d24b447ba9b918df490c
Vivianluolita <15071362462@163.com>
2019-03-19 10:14:36 +0800

    更换svg图片

---
+++ src/components/form/form.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="content">
+        <el-row>
+        <el-col :span="24"><div class="grid-content bg-purple-dark">dsa1da1s111</div></el-col>
+        </el-row>
+    </div>    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
